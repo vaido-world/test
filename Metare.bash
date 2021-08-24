e3aa439ddfd3028337f1b4664eb48afbb2d1fa4a
Feldwor <BoQsc@users.noreply.github.com>
2021-08-24 18:33:15 +0000

    Update Metare.bash

--- Metare.bash
+++ Metare.bash
@@ -1,5 +1,5 @@
 Compile "anything"
-curl -O https://github.com/vaido-world/test/raw/main/Metare.tar.gz
+curl -L -O https://github.com/vaido-world/test/raw/main/Metare.tar.gz
 tar -xf Metare.tar.gz -C /Data/Compile/Recipes
 curl vaido.world/gobo/unionfs.bash | bash
 Compile Metare
