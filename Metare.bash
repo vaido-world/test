0d8f116d91451488b34cc4f4da1c0d23aea5aeea
Feldwor <BoQsc@users.noreply.github.com>
2021-08-24 18:56:54 +0000

    Update Metare.bash

--- Metare.bash
+++ Metare.bash
@@ -1,6 +1,6 @@
 Compile "anything"
 curl -L -O https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/XFConf.tar.gz
-tar -xf XfConf.tar.gz - C /Data/Compile/Recipes
+tar -xf XfConf.tar.gz -C /Data/Compile/Recipes
 curl -L -O https://github.com/vaido-world/test/raw/main/Metare.tar.gz
 tar -xf Metare.tar.gz -C /Data/Compile/Recipes
 curl vaido.world/gobo/unionfs.bash | bash
