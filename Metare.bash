f18b04f69757a6cb508928cda8262dd09165ece6
Feldwor <BoQsc@users.noreply.github.com>
2021-08-25 10:14:32 +0300

    Update Metare.bash

--- Metare.bash
+++ Metare.bash
@@ -11,6 +11,7 @@ tar --extract --file="XFConf.tar.gz" --directory="/Data/Compile/Recipes"
 InstallPackage https://gobolinux.org/packages/017/Fuse--2.9.7--x86_64.tar.bz2
 InstallPackage https://gobolinux.org/packages/017/UnionFS-Fuse--2.1--x86_64.tar.bz2
 
+# The Recipe
 curl --location "https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/XFCE-Meta-Stable.tar.gz" --remote-name
 tar --extract --file="XFCE-Meta-Stable.tar.gz" --directory="/Data/Compile/Recipes"
 
