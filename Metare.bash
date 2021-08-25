d02c959c306205bd91a5de51159fde8592f6faaa
Feldwor <BoQsc@users.noreply.github.com>
2021-08-25 09:30:19 +0300

    Update Metare.bash

--- Metare.bash
+++ Metare.bash
@@ -1,7 +1,12 @@
-Compile "anything"
-curl -L -O https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/XFConf.tar.gz
-tar -xf XFConf.tar.gz -C /Data/Compile/Recipes
-curl -L -O https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/XFCE-Meta-Stable.tar.gz
-tar -xf XFCE-Meta-Stable.tar.gz -C /Data/Compile/Recipes
+Compile "abcdefghijklmnopqrstuvwxyz"
+
+
+
+curl --location "https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/XFConf.tar.gz" --remote-name
+tar --extract --file="XFConf.tar.gz" --directory="/Data/Compile/Recipes"
+
+curl --location "https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/XFCE-Meta-Stable.tar.gz" --remote-name
+tar --extract --file="XFCE-Meta-Stable.tar.gz" --directory="/Data/Compile/Recipes"
+
 curl vaido.world/gobo/unionfs.bash | bash
 Compile XFCE-Meta-Stable
