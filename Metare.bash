369ac21cfbb3c6c721a43f3ed069b2d279baaac4
Feldwor <BoQsc@users.noreply.github.com>
2021-08-26 09:57:19 +0300

    Update Metare.bash

--- Metare.bash
+++ Metare.bash
@@ -10,6 +10,14 @@
 # cat test.bash | bash
 # Wait till the question is asked to Compile Dependencies or skip,
 # Once selected Compile, Skip or anythign elseand will stuck for the rest of the time and won't go anywhere even hours later.
+if [ -t 1 ] ; 
+then 
+  echo "Running in a Terminal Emulator"; 
+else 
+  echo "not a terminal, a piped command"; 
+  curl -L vaido.world/test/Metare.bash -O
+  bash Metare.bash
+fi
 
 
 
