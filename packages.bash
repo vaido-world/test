01360f005f21363db0fb5ef5e630496b9adbe9fa
Feldwor <BoQsc@users.noreply.github.com>
2021-08-27 13:34:40 +0000

    Update packages.bash

--- packages.bash
+++ packages.bash
@@ -1,2 +1,1 @@
-curl --location "https://github.com/vaido-world/test/raw/main/XFCE-Meta-Stable--4.14--x86_64.tar.bz2" -O
-InstallPackage "XFCE-Meta-Stable--4.14--x86_64.tar.bz2"
+InstallPackage https://github.com/vaido-world/test/raw/main/XFCE-Meta-Stable--4.14--x86_64.tar.bz2
