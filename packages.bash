f7a505f34bbfa10caed1317c3450676af20bf6f9
Feldwor <BoQsc@users.noreply.github.com>
2021-08-27 13:40:38 +0000

    Update packages.bash

--- packages.bash
+++ packages.bash
@@ -1,2 +1,12 @@
 InstallPackage "https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/Package-latest/Dependencies/LibWNCK--3.36.0--x86_64.tar.bz2"
+InstallPackage "https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/Package-latest/Dependencies/LibGUdev--234--x86_64.tar.bz2"
+InstallPackage "https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/Package-latest/Dependencies/UPower--0.99.11--x86_64.tar.bz2"
 InstallPackage "https://github.com/vaido-world/test/raw/main/XFCE-Meta-Stable--4.14--x86_64.tar.bz2"
+
+InstallPackage "https://github.com/vaido-world/XFCE4-GoboLinux-Future/raw/main/XFCE4-Screenshooter/XFCE4-Screenshooter--1.8.99--x86_64.tar.bz2"
+
+InstallPackage "https://github.com/vaido-world/XFCE4-GoboLinux-Future/blob/main/XFCE4-Terminal/VTE--0.57.90--x86_64.tar.bz2"
+InstallPackage "https://github.com/vaido-world/XFCE4-GoboLinux-Future/raw/main/XFCE4-Terminal/XFCE4-Terminal--0.8.9--x86_64.tar.bz2"
+
+InstallPackage "https://github.com/vaido-world/XFCE4-GoboLinux-Future/raw/main/Mousepad/GtkSourceView--3.22.0--x86_64.tar.bz2"
+InstallPackage "https://github.com/vaido-world/XFCE4-GoboLinux-Future/raw/main/Mousepad/Mousepad--0.4.2--x86_64.tar.bz2"
