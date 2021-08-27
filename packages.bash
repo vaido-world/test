ef0bcc01240344935c8977b72e2709038c01824b
Feldwor <BoQsc@users.noreply.github.com>
2021-08-27 13:36:11 +0000

    Update packages.bash

--- packages.bash
+++ packages.bash
@@ -1,1 +1,2 @@
-InstallPackage https://github.com/vaido-world/test/raw/main/XFCE-Meta-Stable--4.14--x86_64.tar.bz2
+InstallPackage "https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/Package-latest/Dependencies/LibWNCK--3.36.0--x86_64.tar.bz2"
+InstallPackage "https://github.com/vaido-world/test/raw/main/XFCE-Meta-Stable--4.14--x86_64.tar.bz2"
